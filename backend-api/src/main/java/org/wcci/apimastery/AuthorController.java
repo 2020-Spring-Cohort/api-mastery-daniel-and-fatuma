33a594375ac342e60fdf93615bf6fb1775ff53b7
DanielVillasmil <dvillasmil08@gmail.com>
2020-03-17 18:05:57 -0400

    Created Populator

--- backend-api/src/main/java/org/wcci/apimastery/AuthorController.java
+++ backend-api/src/main/java/org/wcci/apimastery/AuthorController.java
@@ -1,19 +1,23 @@
 package org.wcci.apimastery;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
 @RestController
+@RequestMapping("authors")
+
 public class AuthorController {
     private AuthorRepository authorRepository;
 
     public AuthorController(AuthorRepository authorRepository){
         this.authorRepository = authorRepository;
     }
-    @RequestMapping("/authors")
+    @GetMapping("")
     public Collection<Author> retrieveAuthors(){
         return(Collection<Author>) authorRepository.findAll();
     }
+
 }
